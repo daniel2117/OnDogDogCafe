de9d4cca5b834c7b9c5f8ad63d6b623dabe6ed0d
Jeff3c <jeff3c@connect.hku.hk>
2025-03-18 22:29:00 +0800

    add other essential components
    .env, middleware, file names updated

--- dog-cafe-backend/server.js
+++ dog-cafe-backend/server.js
@@ -9,5 +9,7 @@ connectDB();
 
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/reservations", require("./routes/reservationRoutes"));
-
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 app.listen(5000, () => console.log("Server running on port 5000"));
